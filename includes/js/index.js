2e600eb948ce0698041e083ce315f415836743c8
SpreceGames <renato_sprece@hotmail.com>
2026-09-24 14:33:18 -0300

    Update index.js

--- includes/js/index.js
+++ includes/js/index.js
@@ -20,7 +20,7 @@ var rtlLangs = ["ar", "fa"];
 
 const webKitMin = 6.70;
 const webKitMax = 13.52;
-const projectName = "WebKitty";
+const projectName = "DIGA NÃO A PIRATARIA, ESSE MATERIAL É APENAS PARA ESTUDOS!";
 
 const ui = {
   mainContainer: document.querySelector('.mainContainer'),
